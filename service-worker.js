8aa22a9cdcbf6fdc9e92c2b9a931ffc8417c8e0b
Ethereon <acostaemmanuel365@gmail.com>
2026-02-17 19:05:13 -0300

    cambios

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,15 @@
-const CACHE = "recetas-cache-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./script.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.open("app-v1").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
   );
 });
